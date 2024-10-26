8258f3f92d04420437c37c629ae3ad16fe301f6a
Liziero <iagolizierox14@gmail.com>
2024-10-26 14:20:48 -0300

    primeiro commit, corrigir alguns detalhes

---
+++ script.js
@@ -0,0 +1,29 @@
+function calculo() {
+    //captura as variáveis
+    let peso = Number(document.getElementById('peso').value);
+    let altura = Number(document.getElementById('altura').value);
+    let idade = Number(document.getElementById('idade').value);
+    let operacao = (document.getElementById('operacao').value);
+    let resultadoSaida = document.getElementById('resultadoSaida');
+    let resultado;
+     
+    //caso for um valor inválido
+    if(peso === "" || altura === "" || idade === "" || isNaN(idade) || isNaN(altura) || isNaN(peso)) {
+        alert("insira um número válido")  
+        return;  
+    }
+    //swich para escolher as operacoes
+    switch(operacao) {
+        case "homem": 
+        resultado = 88.36 + (13.4 * peso) + (4.8 * altura) - (5.7 * idade);
+        break;
+        case "mulher":
+        resultado = 447.6 + (9,2 * peso) + (3.1 * altura) - (4.3 * idade);
+        break;
+        default:
+        alert("Insira um número valido.")
+        break;
+    }
+    //resultado
+    resultadoSaida.textContent = "Sua Taxa Metabólica Basal é: " + resultado;
+}
